a781af1c29ebfac3e49df9547c7761814ec6886d
suhyyyuuunnn <dl7521216@naver.com>
2021-02-17 23:03:12 +0900

    commit

--- Test/settings.py
+++ Test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't(y93@5qf1j_#*7(+a6w26+iu6woukf2f2=p^_purq4t$cl533'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
